35fcace077f6baaaea2da46cd01b3a6ef84354fa
sys7375 <juliobenin@yahoo.com.br>
2020-07-22 10:31:37 -0300

    removido .gitignore de modelos

---
+++ modelo/env.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+Arquivo de configuração do ambiente
+*/
+
+include('../funcoes.php');
+include('../s_acessos.php');
+
+$sql = "SELECT * FROM pagseguro_configuracao ";
+$result = mysql_query($sql);
+
+if ((mysql_num_rows($result) > 0)) {
+	$row = mysql_fetch_assoc($result);
+	$emailPagseguro = $row["email"];
+	$tokenPagseguro = $row["token"];
+	$urlNotificacao = $row["urlnotificacao"];
+	$sandBox = $row['sandbox'];
+}
+
+if ($sandBox != 1) {
+	$scriptPagseguro = "https://stc.pagseguro.uol.com.br/pagseguro/api/v2/checkout/pagseguro.directpayment.js";
+	$urlPagseguro = "https://ws.pagseguro.uol.com.br/v2/";
+} else {
+	$scriptPagseguro = "https://stc.sandbox.pagseguro.uol.com.br/pagseguro/api/v2/checkout/pagseguro.directpayment.js";
+	$urlPagseguro = "https://ws.sandbox.pagseguro.uol.com.br/v2/";
+}
